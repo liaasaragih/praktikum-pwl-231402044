7ea291f51f36869046cb43cf79387ce40bd0bb06
liaasrg <liaadamayana@gmail.com>
2024-02-29 09:42:39 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <p class="text-3xl bg-red-500 ">lia</p>
+    <div class="card w-96 bg-base-100 shadow-xl">
+  <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+      <button class="btn bg-green-600 hover:bg-red-500">buy now</button>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
